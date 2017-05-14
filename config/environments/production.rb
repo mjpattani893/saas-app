4d0dc719df90d79a5b6d28461ded66b25443c56a
Maitreyi <maitreyipattani@gmail.com>
2017-05-14 04:37:10 -0700

    Add Users Project relationship

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,5 +100,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'saas-app-mjp.herokuapp.com', :protocol => 'https'}
-
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "saas-app-mjp.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+}
 end
